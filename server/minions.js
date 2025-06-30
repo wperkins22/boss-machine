7144a30cbb4a1692f0c21cf653347bd72c24339b
Wyatt Perkins <perkinswyatt@gmail.com>
2025-06-30 12:18:41 -0600

    add routes for minions operations

---
+++ server/minions.js
@@ -0,0 +1,49 @@
+const minionsRouter = require('express').Router();
+
+module.exports = minionsRouter;
+
+const {
+    getAllFromDatabase,
+    getFromDatabaseById,
+    addToDatabase,
+    updateInstanceInDatabase,
+    deleteFromDatabasebyId
+} = require('./db');
+
+minionsRouter.param('minionId', (req, res, next, id) => {
+    const minion = getFromDatabaseById('minions', id);
+    if (!minion) {
+        res.status(404).send();
+    } else {
+        req.minion = minion;
+        next();
+    }
+});
+
+minionsRouter.get('/', (req, res, next) => {
+    res.send(getAllFromDatabase('minions'));
+});
+
+minionsRouter.post('/', (req, res, next) => {
+    const newMinion = addToDatabase('minions', req.body);
+    res.status(201).send(newMinion);
+});
+
+minionsRouter.get('/:minionId', (req, res, next) => {
+    res.send(req.minion);
+});
+
+minionsRouter.put('/:minionId', (req, res, next) => {
+    let updatedMinion = updateInstanceInDatabase('minions', req.body);
+    res.send(updatedMinion);
+});
+
+minionsRouter.delete('/:minionId', (req, res, next) => {
+    const deletedMinion = deleteFromDatabasebyId('minions', req.params.minionId);
+    if (deletedMinion) {
+        res.status(204);
+    } else {
+        res.status(500);
+    }
+    res.send();
+});
